71453177b8772907a75a4a92cbb0fb7bdbf090d0
Halite Abudureyimu <haliteabudureyimu@gmail.com>
2019-01-26 22:46:20 +0000

    update wiki

--- backend/wiki.js
+++ backend/wiki.js
@@ -4,17 +4,39 @@ const rp = require('request-promise');
 const textToSpeechAPI = 'd5537ddec597441e982b802e882dfc44';
 const fs = require('fs');
 const request = require('request');
+const moment=require('moment');
+const util = require('util');
+const textToSpeech = require('@google-cloud/text-to-speech');
+const Audio = require('./schemas/audio')
 
-function createIntroAudio(loc) {
-    let searchString = "https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=true&prop=extracts&explaintext=1&titles=" + loc;
-    client.get(searchString, '', function (data, response) {
+function createIntroAudio(locationName, locationID) {
+    let searchString = "https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=true&prop=extracts&explaintext=1&titles=" + locationName;
+    const audio = new Audio();
+    audio.Date=moment().unix();
+    audio.LocationID=locationID;
+    audio.Artist="Wikipedia";
+    audio.Title=location + " Introduction"
+
+    client.get(searchString, '', async function (data, res) {
         var values = Object.values(data.query.pages);
         const extract = values[0].extract.split('\n')[0];
-        var searchString = 'http://api.voicerss.org/?key=' + textToSpeechAPI + '&hl=en-us&src=' + extract;
-        request.get(searchString).on('error', function(err) {
-          console.log("Couldn't create intro file");
-        }).pipe(fs.createWriteStream('./data/landmarks/' + loc + '/intro.mp3'));
+        
+        const request = {
+            input: {text: extract},
+            // Select the language and SSML Voice Gender (optional)
+            voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
+            // Select the type of audio encoding
+            audioConfig: {audioEncoding: 'MP3'},
+        };
+        const [response] = await client.synthesizeSpeech(request);
+        // Write the binary audio content to a local file
+        const writeFile = util.promisify(fs.writeFile);
+        await writeFile('output.mp3', response.audioContent, 'binary');
+        console.log('Audio content written to file: output.mp3');
+        
     });
+
+
     
 }
 
