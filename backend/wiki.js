7f4805e095d2825e2aed30996c687eb716d5852f
Halite Abudureyimu <haliteabudureyimu@gmail.com>
2019-01-26 22:04:46 +0000

    update wiki

--- backend/wiki.js
+++ backend/wiki.js
@@ -3,23 +3,19 @@ const client = new Client();
 const rp = require('request-promise');
 const textToSpeechAPI = 'd5537ddec597441e982b802e882dfc44';
 const fs = require('fs');
-
+const request = require('request');
 
 function createIntroAudio(loc) {
-    let searchString = "https://en.wikipedia.org/wiki/" + loc;
-    client.get(searchString,'', (data, res)=> {
-        console.log(res);
-        let searchString = "https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=true&prop=extracts&explaintext=1&titles=" + loc;
-        client.get(searchString, '', function (data, response) {
-            var values = Object.values(data.query.pages);
-            const extract = values[0].extract;
-            console.log(extract.split('\n')[0]);
-            var searchString = 'http://api.voicerss.org/?key=' + textToSpeechAPI + '&hl=en-us&src=' + extract;
-            request.get(searchString).on('error', function(err) {
-              console.log("Couldn't create intro file");
-            }).pipe(fs.createWriteStream('./data/landmarks/' + place_id + '/intro.mp3'));
-       });
-    })
+    let searchString = "https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=true&prop=extracts&explaintext=1&titles=" + loc;
+    client.get(searchString, '', function (data, response) {
+        var values = Object.values(data.query.pages);
+        const extract = values[0].extract;
+        console.log(extract.split('\n')[0]);
+        var searchString = 'http://api.voicerss.org/?key=' + textToSpeechAPI + '&hl=en-us&src=' + extract;
+        request.get(searchString).on('error', function(err) {
+          console.log("Couldn't create intro file");
+        }).pipe(fs.createWriteStream('./data/landmarks/' + loc + '/intro.mp3'));
+    });
     
 }
 
