0536cfae1e114932c50332bcf502e53a225b1170
Halite Abudureyimu <haliteabudureyimu@gmail.com>
2019-01-26 13:56:52 +0000

    Added create user function

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
-
+const sjcl = require("sjcl");
 const API_PORT = 3001;
 const app = express();
 const router = express.Router();
@@ -37,4 +37,31 @@ app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
 
 router.get("/", (req, res) => {
     console.log("SUCCESSFUL GET REQUEST");
+})
+
+router.post("/newUser", (req, res) =>{
+    let user = new User();
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const password;
+    sjcl.encrypt(password, req.body.password);
+    // TODO: Test the complexcity of password
+    
+    const emailAddress = req.body.emailAddress;
+    // TODO: Check the uniqueness of the email address
+
+    if (!firstName || !lastName || !emailAddress) {
+        return res.json({
+            first:req.body,
+            success: false,
+            error:"Invalid Inputs"
+        });
+    }
+    user.FirstName = firstName;
+    user.LastName = lastName;
+    user.Email = emailAddress;
+    user.Password = password;
+    user.save(err => {
+        return res.json({success: err != null, error: err})
+    })
 })
