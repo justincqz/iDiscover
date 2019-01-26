7fa189b782b1569288ed31fe4c4f1e9513c84aec
halid1020 <46388963+halid1020@users.noreply.github.com>
2019-01-26 22:01:01 +0000

    Update wiki.js

--- backend/wiki.js
+++ backend/wiki.js
@@ -9,17 +9,15 @@ function createIntroAudio(loc) {
     let searchString = "https://en.wikipedia.org/wiki/" + loc;
     client.get(searchString,'', (data, res)=> {
         console.log(res);
-        let searchString = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext=1&titles=" + loc;
+        let searchString = "https://en.wikipedia.org/w/api.php?format=json&action=query&redirects=true&prop=extracts&explaintext=1&titles=" + loc;
         client.get(searchString, '', function (data, response) {
             var values = Object.values(data.query.pages);
             const extract = values[0].extract;
             console.log(extract.split('\n')[0]);
             var searchString = 'http://api.voicerss.org/?key=' + textToSpeechAPI + '&hl=en-us&src=' + extract;
-            /*
             request.get(searchString).on('error', function(err) {
               console.log("Couldn't create intro file");
             }).pipe(fs.createWriteStream('./data/landmarks/' + place_id + '/intro.mp3'));
-            */
        });
     })
     
